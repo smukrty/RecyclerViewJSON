318982b0a45840d92e2cf4c0eb098882c999aec2
smukrty <smukrty50711@gmail.com>
2021-05-19 13:28:43 +0800

    Initial commit

---
+++ app/src/main/java/com/test/recyclerviewjson/ExampleItem.java
@@ -0,0 +1,21 @@
+package com.test.recyclerviewjson;
+
+public class ExampleItem {
+    private String mImageUrl;
+    private String mCreator;
+    private int mLikes;
+    public ExampleItem(String imageUrl, String creator, int likes) {
+        mImageUrl = imageUrl;
+        mCreator = creator;
+        mLikes = likes;
+    }
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+    public String getCreator() {
+        return mCreator;
+    }
+    public int getLikeCount() {
+        return mLikes;
+    }
+}
